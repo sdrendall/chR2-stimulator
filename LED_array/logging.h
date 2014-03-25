51daf4a6bc65b7d6a22312145fea2f2a42ab3326
Sam Rendall <sdrendall@gmail.com>
2014-03-25 08:26:29 -0400

    moved code to .c

--- LED_array/logging.h
+++ LED_array/logging.h
@@ -1,35 +1,4 @@
-#include "Arduino.h"
-
-unsigned long startTime;
-
-// Logging Functions
-void logEvent(String msg) {
-  Serial.print("[TIME]: ");
-  Serial.print(millis());
-  Serial.print(" [LOG]: ");
-  Serial.println(msg);
-}
-
-void debugOut(String msg) {
-  Serial.print("[TIME]: ");
-  Serial.print(millis());
-  Serial.print(" [DEBUG]: ");
-  Serial.println(msg);
-}
-
-void errOut(String err) {
-  Serial.print("[TIME]: ");
-  Serial.print(millis());
-  Serial.print(" [ERROR]: ");
-  Serial.println(err);
-} 
-
-void document(String parameter, float value) {
-  if (parameter == "start") {
-    startTime = millis();
-  }
-  Serial.print("data ");
-  Serial.print(millis() - startTime);
-  Serial.print(String(" ") + parameter + String(" "));
-  Serial.println(value);
-}
+void logEvent(String msg);
+void debugOut(String msg);
+void errOut(String err);
+void document(String parameter, float value);
