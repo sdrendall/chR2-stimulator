e4de5636c2ccc5cf305a0ec5f67e7588b42623cf
Sam Rendall <sdrendall@gmail.com>
2014-03-26 10:56:41 -0400

    Changed parameters, numBlocks was greater than the length of experiment arrays, causing a bug

--- LED_array/arrayControl.cpp
+++ LED_array/arrayControl.cpp
@@ -24,14 +24,14 @@ void turnLEDOff(int led) {
     // Incompatible with PWM.  Assumes that LEDs are only being controlled
     // With digitalWrite()
     digitalWrite(gatePins[led], HIGH);
-    document("led", 0);
+    document(String("led"), 0);
     ledOn[led] = false;
 }
 
 void turnLEDOn(int led) {
     // Doesn't involve PWM.  Turns the LED all the way on
     digitalWrite(gatePins[led], LOW);
-    document("led", 1);
+    document(String("led"), 1);
     ledOn[led] = true;
 }
 
@@ -58,7 +58,7 @@ void scheduleNextLEDEvent(int led) {
 }
 
 void toggleLED(int led) {
-    if (activeExperiment || manualMode) {
+    if (isBursting[led]) {
         scheduleNextLEDEvent(led);
     }
     if (ledOn[led]) {
@@ -72,7 +72,7 @@ void checkForLEDEvents() {
     // loop through LEDs
     for(int led = 0; led < numLEDs; led++) {
         // Toggle LED when necessary, uses us
-        if ((activeExperiment || manualMode) && micros() >= nextLEDEvent[led]) {
+        if (isBursting[led]) && micros() >= nextLEDEvent[led]) {
             toggleLED(led);
         }
     }
@@ -80,29 +80,13 @@ void checkForLEDEvents() {
 
 
 
-// Resets the led's triggerDelay based on its
-// currPulseWidth and currFreq
-void updateTriggerDelay(int led) {
-    currTriggerDelay[led] = calculateTriggerDelay(currFreq[led], currPulseWidth[led]);
-}
-
-// returns the appropriate trigger delay (in us) based on the given freq and pw
-unsigned long calculateTriggerDelay(float freq, unsigned long pw) {
-    // Hz to ms
-    unsigned long tFreq = (1/freq)*1000;
-    // ms to us
-    unsigned long period = tFreq*1000;
-    // Calculate triggerDelay
-    unsigned long triggerDelay = period - pw;
-    return triggerDelay;
-}
-
-
 // --- BURST LOGIC ---
 void checkForBurstEvents() {
-    for (int led = 0; led < numLEDs; led++) {
-        if ((activeExperiment || manualMode) && millis() >= nextBurstEvent[led]) {
-            toggleBurstState(led);
+    if ((activeExperiment || manualMode) {
+        for (int led = 0; led < numLEDs; led++) {
+             if millis() >= nextBurstEvent[led]) {
+                toggleBurstState(led);
+            }
         }
     }
 }
@@ -120,16 +104,16 @@ void scheduleNextBurstEvent(int led) {
     // Schedule then next burst event, dependent on the led's bursting state
     // Designed to be immediately followed by a call to stopBursting() or startBursting()
     if (isBursting[led]) {
-        nextBurstEvent[led] = micros() + currBurstInterim[led];
+        nextBurstEvent[led] = millis() + currBurstInterim[led];
     } else {
-        nextBurstEvent[led] = micros() + currBurstDuration[led];
+        nextBurstEvent[led] = millis() + currBurstDuration[led];
     }
 } 
 
 void startBursting(int led) {
     // Start new LED train
-    toggleLED(led);
     isBursting[led] = true;
+    toggleLED(led);
 }
 
 void stopBursting(int led) {
