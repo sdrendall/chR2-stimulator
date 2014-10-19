c8155ca9358fdf6d361d97dfab7b8cb0b486b133
Kelsey Tyssowski <kelseytyssowski@kelseys-mbp-2.private.wireless.med.harvard.edu>
2014-10-19 16:39:49 -0400

    Added Added Estim_strobe.ino, which switches LED_strobe.ino to work with Estim (i.e. prevents sending current when Estim should be off)

--- I-O_display.py
+++ I-O_display.py
@@ -10,7 +10,7 @@ import os, sys, datetime
 import subprocess as sp
 
 # The serial port the teensy is connected to
-teensyPort = "/dev/tty.usbmodem12341"
+teensyPort = "/dev/tty.usbmodem92681"
 
 # The Root object, he ties it all together
 class Root(object):
